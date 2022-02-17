981d91585a2985cc825602720af0d75ce2941f91
kons <sebastian.kons@tu-dortmund.de>
2022-02-17 13:19:19 +0100

    clustering routine

--- polycorr/plotting.py
+++ polycorr/plotting.py
@@ -1,9 +1,26 @@
 """Plot fibers and strains"""
 
+from cProfile import label
+from matplotlib import projections
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+from matplotlib.cm import ScalarMappable
+import matplotlib.ticker as mtick
 from random import randint
+import os
+
+from collections import Counter
+
+DARK2 = [(217 / 255, 95 / 255, 2 / 255),
+         (27 / 255, 158 / 255, 119 / 255),
+         (117 / 255, 112 / 255, 179 / 255),
+         (231 / 255, 41 / 255, 138 / 255),
+         (102 / 255, 166 / 255, 30 / 255),
+         (230 / 255, 171 / 255, 2 / 255),
+         (166 / 255, 118 / 255, 29 / 255),
+         (102 / 255, 102 / 255, 102 / 255)]
+
 
 def plot_fibers(points, segments):
     """Method to plot fibers based on points and segments"""
@@ -20,8 +37,223 @@ def plot_fibers(points, segments):
     collection = mplot3d.art3d.Line3DCollection(lines, colors=colors)
     axs.add_collection(collection)
     points = np.transpose(points)
-    axs.set_xticks(np.arange(min(points[0]), max(points[0]), (max(points[0])-min(points[0]))//5))
-    axs.set_yticks(np.arange(min(points[1]), max(points[1]), (max(points[1])-min(points[1]))//5))
-    axs.set_zticks(np.arange(min(points[2]), max(points[2]), (max(points[2])-min(points[2]))//5))
+    axs.set_xticks(
+        np.arange
+        (min(points[0]), max(points[0]), (max(points[0])-min(points[0]))//5))
+    axs.set_yticks(
+        np.arange(
+            min(points[1]), max(points[1]),
+            (max(points[1])-min(points[1]))//5))
+    axs.set_zticks(
+        np.arange(
+            min(points[2]), max(points[2]),
+            (max(points[2])-min(points[2]))//5))
 
     plt.show()
+
+
+def plot_fibers_clustering(points, segments, clustering):
+    """Method to plot fibers based on points and segments"""
+
+    fig = plt.figure()
+    axs = fig.add_subplot(111, projection='3d')
+
+    lines = []
+    for segment in segments:
+        lines.append([points[idx] for idx in segment])
+
+    sm = ScalarMappable(cmap="gist_ncar")
+    colors = sm.to_rgba(clustering)
+
+    collection = mplot3d.art3d.Line3DCollection(
+        lines, colors=colors)
+    axs.add_collection(collection)
+    points = np.transpose(points)
+    axs.set_xticks(
+        np.arange
+        (min(points[0]), max(points[0]), (max(points[0])-min(points[0]))//5))
+    axs.set_yticks(
+        np.arange(
+            min(points[1]), max(points[1]),
+            (max(points[1])-min(points[1]))//5))
+    axs.set_zticks(
+        np.arange(
+            min(points[2]), max(points[2]),
+            (max(points[2])-min(points[2]))//5))
+
+    plt.show()
+
+
+def plot_most_common_clusters(clustering, metric):
+    labels, values = zip(*Counter(clustering).most_common(10))
+
+    indexes = np.arange(len(labels))
+    width = 0.5
+
+    plt.bar(indexes, values, width)
+    plt.xticks(indexes, labels)
+    plt.title(
+        f"HDBSCAN with {metric} disctance. Num. Clusters:"
+        f"{len(np.unique(clustering))}")
+    plt.savefig(f"results/hdbscan_{metric}.png", dpi=300)
+
+
+def plot_scores(scores, optimal_min_size):
+
+    plt.plot(np.arange(2, len(scores) + 2), scores, 'o', ms=4, c="black")
+    plt.plot(np.arange(2, len(scores) + 2), scores, c="black")
+    plt.xticks(
+        np.arange(10, len(scores) + 2, 10), np.arange(10, len(scores) + 2, 10))
+    plt.ylabel("score")
+    plt.xlabel("min size of clusters")
+    plt.title(f"optimal min size of clusters: {optimal_min_size}")
+    plt.grid(True)
+    plt.savefig("results/scores.png", dpi=300)
+
+
+def plot_num_clusters(nums):
+
+    plt.plot(np.arange(2, len(nums) + 2), nums, 'o', ms=4, c="black")
+    plt.plot(np.arange(2, len(nums) + 2), nums, c="black")
+    plt.xticks(
+        np.arange(10, len(nums) + 2, 10), np.arange(10, len(nums) + 2, 10))
+    plt.ylabel("score")
+    plt.xlabel("min size of clusters")
+    plt.title(f"number of clusters per min size of clusters")
+    plt.grid(True)
+    plt.savefig("results/num_clusters.png", dpi=300)
+
+
+def plot_num_clusters_vs_scores(scores, nums):
+
+    plt.plot(np.arange(2, len(nums) + 2), nums, 'o', ms=4, c=DARK2[0])
+    plt.plot(
+        np.arange(2, len(nums) + 2), nums, c=DARK2[0], label="num of clusters")
+    plt.plot(
+        np.arange(2, len(scores) + 2), scores * 1000, 'o', ms=4, c=DARK2[1])
+    plt.plot(
+        np.arange(2, len(scores) + 2),
+        scores * 1000, c=DARK2[1], label="score e+2")
+    plt.xticks(
+        np.arange(10, len(nums) + 2, 10), np.arange(10, len(nums) + 2, 10))
+    plt.xlabel("min size of clusters")
+    plt.title(f"number of clusters vs score")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("results/num_clusters_vs_scores.png", dpi=300)
+
+
+def plot_metrics(scores, nums, variances):
+
+    plt.plot(np.arange(2, len(nums) + 2), nums, 'o', ms=4, c=DARK2[0])
+    plt.plot(
+        np.arange(2, len(nums) + 2), nums, c=DARK2[0], label="num of clusters")
+    plt.plot(
+        np.arange(2, len(scores) + 2), scores * 1000, 'o', ms=4, c=DARK2[1])
+    plt.plot(
+        np.arange(2, len(scores) + 2),
+        scores * 1000, c=DARK2[1], label="score * 2e+2")
+    plt.plot(
+        np.arange(2, len(variances) + 2),
+        variances * 1e-9, 'o', ms=4, c=DARK2[2])
+    plt.plot(
+        np.arange(2, len(variances) + 2),
+        variances * 1e-9, c=DARK2[2], label="variances * 1e-9")
+    plt.xticks(
+        np.arange(10, len(nums) + 2, 10), np.arange(10, len(nums) + 2, 10))
+    plt.xlabel("min size of clusters")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("results/metrics.png", dpi=300)
+
+
+def compare3d(strain, clustering, idxs=None, title=""):
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121, projection="3d")
+    ax2 = fig.add_subplot(122, projection="3d")
+    ax1.scatter(*strain[:3], c=strain[-1])
+    ax1.tick_params(
+        left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax1.set_title("strain", y=-0.1)
+
+    if idxs is not None:
+        strain = strain[:, idxs]
+    ax2.scatter(*strain[:3], c=clustering, cmap="gist_ncar")
+    ax2.tick_params(
+        left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax2.set_title("clustering", y=-0.1)
+
+    plt.tight_layout()
+    fig.suptitle(
+        f"BIRCH clustering with threshold = {title}", y=0.9)
+    plt.savefig(f"results/Birch/compare3d{title}.png", dpi=300)
+
+
+def compare3d_fibers(strain, point_clustering, fibers, fiber_clustering,
+                     points,  clustering_name, param_key, param_val,
+                     idxs=None):
+
+    sm = ScalarMappable(cmap="gist_ncar")
+    colors = sm.to_rgba(np.unique(point_clustering))
+
+    point_colors = [colors[idx] for idx in point_clustering]
+    fiber_colors = [colors[idx] for idx in fiber_clustering]
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(131, projection="3d")
+    ax2 = fig.add_subplot(132, projection="3d")
+    ax3 = fig.add_subplot(133, projection="3d")
+
+    ax1.scatter(*strain[:3], c=strain[-1])
+    ax1.tick_params(
+        left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax1.set_title("strain", y=-0.1)
+
+    if idxs is not None:
+        strain = strain[:, idxs]
+    ax2.scatter(*strain[:3], c=point_colors)
+    ax2.tick_params(
+        left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax2.set_title("point clustering", y=-0.1)
+
+    collection = mplot3d.art3d.Line3DCollection(
+        fibers, colors=fiber_colors)
+    ax3.add_collection(collection)
+    ax3.tick_params(
+        left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax3.set_xticks(ax2.get_xticks())
+    ax3.set_yticks(ax2.get_yticks())
+    ax3.set_zticks(ax2.get_zticks())
+    ax3.set_title("fiber clustering", y=-0.1)
+    plt.axis('auto')
+
+    plt.tight_layout()
+    fig.suptitle(
+        f"{clustering_name} with {param_key} = {param_val}", y=0.9)
+    path = f"results/plots/{clustering_name}"
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    plt.savefig(
+        f"{path}"
+        f"/{clustering_name}_-{param_key}_{param_val}"
+        "_compare3d.png", dpi=300)
+
+
+def plot_confidence(probabilities, thresholds):
+
+    fig, axs = plt.subplots(1, 1, sharex=False, sharey=False)
+    axs.boxplot(
+        probabilities,
+        boxprops=dict(color=DARK2[0]),
+        medianprops=dict(color=DARK2[1], linewidth=2),
+        meanprops=dict(marker="*", markerfacecolor="w", markeredgecolor='k'),
+        showfliers=False,
+        showmeans=True)
+    axs.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1.0))
+    axs.set_xlabel("Threshold")
+    axs.set_ylabel("Confidence of fiber clustering")
+    axs.set_xticklabels(thresholds)
+
+    fig.suptitle("Confidence of BIRCH fiber clustering")
+    plt.savefig("BIRCH_confidence.png", dpi=300)
--- scripts/analyze_correlation.py
+++ scripts/analyze_correlation.py
@@ -1,58 +1,422 @@
 """Script for parsing and plotting fibers"""
 
-from plot_utils import hist
+# from plot_utils import hist
+import os
+from typing import Collection
+from matplotlib import projections
+from matplotlib.cm import ScalarMappable
 
 import numpy as np
-from polycorr.parse_xml import parse_xml
-from polycorr.plotting import plot_fibers
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import hdbscan
+from sklearn import cluster
+from sklearn.manifold import TSNE
 
-from sklearn.cluster import AgglomerativeClustering
+import pickle
+from collections import Counter
+from tqdm import tqdm
+import time
+
+from sklearn.cluster import AgglomerativeClustering, Birch, OPTICS, DBSCAN
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import MinMaxScaler
+
+
+def save_clustering(clustering, filename):
+
+    path = f"results/clusterings/{clustering.__class__.__name__}"
+
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    with open(f"{path}/{filename}.pkl", "wb") as out_file:
+        pickle.dump(clustering, out_file)
+
+
+def eval_fiber_clustering(segments, clustering):
+
+    most_common_clusters = []
+    probabilities = []
+
+    for segment in segments:
+
+        clusters = clustering[segment]
+        most_common_cluster = Counter(clusters).most_common(1)[0][0]
+        probability = Counter(clusters).most_common(1)[0][1] / len(clusters)
+        most_common_clusters.append(most_common_cluster)
+        probabilities.append(probability)
+
+    return most_common_clusters, probabilities
+
 
 def main():
     """Main method"""
+
     specimen = 'TUD_1_2'
     ulimit = 0.0255
     llimit = 0.02
     ubound = 6000
     lbound = 1000
 
-    # points, segments = parse_xml(f'data/fiber/{specimen}.xml', ubound, lbound)
-    # plot_fibers(points, segments)
+    # 1. Load Data
+    # 2. Cluster Data with different
+    #       a) Clustering algorithms
+    #       b) Hyperparameters
+    # 3. Save clusterings
+    # 4. Evaluate fiber clustering
+    # 5. Plot
+    #       a) 3D comparison for points and fibers
+    #       b) confidence of fiber clusterings
+    #       c) Num clusters
 
-    strain = np.genfromtxt(
-        f'data/strain/{specimen}/Ezz03.dat',
-        delimiter=' ',
-        skip_header=4,
-        usecols=(0, 1, 2, 3)
-    )
+    # Load segments and points
+    if os.path.isfile("points.pkl") and os.path.isfile("segments.pkl"):
+        print("loading segments and points")
+        with open(f"points.pkl", "rb") as points_file:
+            points = pickle.load(points_file)
+        with open(f"segments.pkl", "rb") as segments_file:
+            segments = pickle.load(segments_file)
+    else:
+        print("parsing points and segments")
+        points, segments = parse_xml(
+            f'data/fiber/{specimen}.xml', ubound, lbound)
+
+    # Load strain
+    if os.path.isfile("strain.npy"):
+        print("loading strain")
+        strain = np.load("strain.npy")
+    else:
+        print("parsing strain")
+        strain = np.genfromtxt(
+            f'data/strain/{specimen}/Ezz03.dat',
+            delimiter=' ',
+            skip_header=4,
+            usecols=(0, 1, 2, 3)
+        )
+        np.save("strain.npy", strain)
 
     # Remove specific entries
+    print("removing specific entries")
     strain = strain[np.where(strain[:, 2] >= lbound // 1000)]
     strain = strain[np.where(strain[:, 2] <= ubound // 1000)]
     strain = strain[np.where(strain[:, 3] < ulimit)]
     strain = strain[np.where(strain[:, 3] > llimit)]
-    print(strain.shape)
 
+    print("scale strain")
+    scaler = MinMaxScaler()
+    strain = scaler.fit_transform(strain)
+
+    clusterings = [
+        # Birch(n_clusters=None),
+        # OPTICS(min_samples=8, n_jobs=-1),  # min_samples = 2*dim
+        # DBSCAN(min_samples=8, n_jobs=-1),  # (Sander et al., 1998)
+        hdbscan.HDBSCAN(core_dist_n_jobs=-1)
+    ]
+
+    params = [
+        # {"threshold": np.arange(0.05, 0.26, 0.05)},
+        # {},
+        # {"eps": np.arange(0.05, 0.19, 0.05)},
+        {"min_cluster_size": np.arange(5, 51, 5)}
+    ]
+
+    sys.setrecursionlimit(10000)
+
+    pbar_clustering = tqdm(clusterings)
+    for idx, clustering in enumerate(pbar_clustering):
+        param = params[idx]
+
+        pbar_clustering.set_postfix_str(
+            f"{clustering.__class__.__name__}")
+
+        if param != {}:
+            key = list(param)[0]
+
+            pbar_params = tqdm(
+                np.round(param[key], 2)
+                if clustering.__class__.__name__ != "HDBSCAN"
+                else [int(k) for k in param[key]],
+                leave=False)
+            for value in pbar_params:
+                if hasattr(clustering, key):
+                    setattr(
+                        clustering,
+                        key,
+                        value
+                    )
+                pbar_params.set_postfix_str(
+                    f"{key} = {getattr(clustering, key)}")
+                clustering.fit(strain)
+                save_clustering(
+                    clustering,
+                    f"{clustering.__class__.__name__}"
+                    f"-{key}_{value}"
+                )
+        else:
+            clustering.fit(strain)
+            save_clustering(
+                clustering,
+                f"{clustering.__class__.__name__}"
+            )
+
+    quit()
+
+    # --- Plotting --- #
+
+    pbar_clustering = tqdm(clusterings)
+    for idx, clustering in enumerate(pbar_clustering):
+        param = params[idx]
+
+        pbar_clustering.set_postfix_str(
+            f"{clustering.__class__.__name__}")
+
+        if param != {}:
+            key = list(param)[0]
+
+            pbar_params = tqdm(
+                np.round(param[key], 2)
+                if clustering.__class__.__name__ != "HDBSCAN"
+                else [int(k) for k in param[key]],
+                leave=False)
+            for value in pbar_params:
+
+    with open(
+     f"results/1try/Birch/Birch_0.1_skaled.pkl", "rb") as clusterer_file:
+        clusterer = pickle.load(clusterer_file)
+
+    line_scaler = MinMaxScaler()
+
+    lines = []
+    for segment in segments:
+        line = np.vstack([points[idx] for idx in segment])
+        line_scaler.partial_fit(line)
+        lines.append(line)
+
+    scales_lines = []
+    for line in lines:
+        line = line_scaler.transform(line)
+        scales_lines.append(line)
+
+    lines = scales_lines
+
+    most_common_clusters, prob = eval_fiber_clustering(
+        segments, clusterer.labels_)
+
+    strain = np.transpose(strain)
+
+    plotting.compare3d_fibers(
+        strain,
+        clusterer.labels_,
+        lines,
+        most_common_clusters,
+        points,
+        "BIRCH",
+        "threshold",
+        0.1
+    )
+
+    quit()
+
+    quit()
+    thresholds = np.round(np.linspace(0.05, 0.2, 4), 2)
+    probablities = []
+    for threshold in tqdm(thresholds):
+
+        with open(f"results/Birch/Birch_{threshold}_skaled.pkl", "rb") as clusterer_file:
+            clusterer = pickle.load(clusterer_file)
+
+        most_common_clusters, prob = eval_fiber_clustering(
+            segments, clusterer.labels_)
+        probablities.append(prob)
+
+    plotting.plot_confidence(probablities, thresholds)
+    quit()
+
+    print(max(probablities))
+    print(min(probablities))
+    print(np.mean(probablities))
+    quit()
+
+    # for i in np.unique(most_common_clusters):
+    #     idxs = np.where(np.array(most_common_clusters) == i)[0]
+    #     segments_c = np.array(segments)[np.array(idxs)]
+    #     clustering_c = np.array(most_common_clusters)[idxs]
+    plotting.plot_fibers_clustering(
+        points,
+        segments,
+        most_common_clusters
+    )
+
+    quit()
+    
+    for cluster in clusterer.labels_:
+
+        idxs = np.where(clusterer.labels_ == cluster)[0]
+        print(idxs)
+
+        plotting.plot_fibers_clustering(
+            points[idxs],
+            segments,
+            clusterer.labels_[idxs]
+        )
+    quit()
+
+    print(len(points))
+    print(len(segments))
+    quit()
+    
+    # with open(f"points.pkl", "wb") as out_file:
+    #     pickle.dump(points, out_file)
+    # with open(f"segments.pkl", "wb") as out_file:
+    #     pickle.dump(segments, out_file)
+    quit()
+    plotting.plot_fibers(points, segments)
+    quit()
+
+
+
+
+    # projection = TSNE(verbose=1, n_jobs=-1).fit_transform(strain)
+    # plt.scatter(*projection.T)
+    # plt.show()
+    # quit()
     # Random sub sample for plot
-    # s_samples = 100000
+    # print("start random sampling...")
+    # s_samples = 100
     # indices = np.arange(0, len(strain))
-    # rand_ids= np.random.choice(indices, s_samples, replace=False)
+    # rand_ids = np.random.choice(indices, s_samples, replace=False)
     # strain = strain[rand_ids]
 
-    # clustering = AgglomerativeClustering().fit(strain)
+    #metrics = list(hdbscan.dist_metrics.METRIC_MAPPING.keys())
+
+    # if not os.path.isfile("clusterer.pkl"):
+    #     print("start clustering...")
+
+    scores = np.empty(49)
+    num_clusters = np.empty(49)
+    variances = np.empty(49)
+
+    # loop = tqdm(range(2, 51))
+    # for i, min_size in enumerate(loop):
+
+
+    #     loop.set_postfix({"min_size": min_size})
+
+
+
+    #for threshold in tqdm(np.arange(0.05, 0.2, 0.05)):
+    # clusterer = Birch(threshold=threshold, n_clusters=None)
+    # clusterer.fit(strain)
+    
+    with open(f"results/Birch/Birch_{threshold}_skaled.pkl", "rb") as clusterer_file:
+        clusterer = pickle.load(clusterer_file)
+        plotting.compare3d(
+            np.transpose(strain), clusterer.labels_, title=threshold)
+
+    
+
+    # with open(f"results/Birch/Birch_{threshold}_skaled.pkl", "wb") as out_file:
+    #     pickle.dump(clusterer, out_file)
+
+
+    # idxs = np.where(strain[:, -1] > 0.99)
+    # strain = strain[idxs]
+
+
+    quit()
+
+    min_size = 7
+
+    clusterer_path = f"results/clusterings/hdbscan_{min_size}.pkl"
+    with open(clusterer_path, "rb") as clusterer_file:
+        clusterer = pickle.load(clusterer_file)
+    fig = plt.figure()
+    axs = fig.add_subplot(projection='3d')
+    print(Counter(clusterer.labels_).most_common(10))
+
+    idxs = np.where((clusterer.labels_ != -1) & (clusterer.labels_ != 46))[0]
+    strain = np.transpose(strain[idxs])
+    plotting.compare3d(strain, clusterer.labels_[idxs], 7)
+    quit()
+
+        # scores[i] = len(
+        #     np.where(clusterer.probabilities_ < 0.05)[0]) / len(
+        #         strain
+        #     )
+        # num_clusters[i] = len(
+        #     np.unique(clusterer.labels_)
+        # )
+
+        # #labels, values = zip(*Counter(clusterer.labels_))
+        # c = Counter(clusterer.labels_)
+        # variances[i] = np.var(list(c.values()))
+
+        # plotting.compare3d(
+        #     np.transpose(strain), clusterer.labels_, min_clusters=min_size)
+    quit()
+
+    #plot_metrics(scores, num_clusters, variances)
+    #plot_num_clusters_vs_scores(scores, num_clusters)
+    #plot_num_clusters(num_clusters)
+    #plot_scores(scores, list(range(2, 51))[np.argmin(scores)])
+
+        # with open(f"results/hdbscan_{min_size}.pkl", "wb") as out_file:
+        #     pickle.dump(clusterer, out_file)
+    # else:
+    #     with open("clusterer.pkl", "rb") as clusterer_file:
+    #         clusterer = pickle.load(clusterer_file)
+
+    quit()
+
+    # print(Counter(clusterer.labels_).most_common(10))
+    # quit()
+    # labels, values = zip(*Counter(clusterer.labels_).most_common(10))
+
+    # indexes = np.arange(len(labels))
+    # width = 1
+
+    # plt.bar(indexes, values, width)
+    # plt.xticks(indexes + width * 0.5, labels)
+    # plt.show()
+    # quit()
+
+    # idxs = [np.where(clusterer.labels_ == i) for i in range(300)]
+    # idxs = np.array(np.hstack(idxs)).flatten()
+
+    idxs = np.where((clusterer.labels_ != -1) & (clusterer.labels_ != 238))[0]
+    idxs = np.where(clusterer.labels_ == -1)[0]
+    fig = plt.figure()
+    axs = fig.add_subplot(projection='3d')
+
+    org_strain = strain
+    #org_strain = np.transpose(org_strain)
+    #plot2 = axs.scatter(org_strain[0], org_strain[1], org_strain[2], c=org_strain[3])
+
+    strain = strain[idxs]
+    strain = np.transpose(strain)
+
+    plot = axs.scatter(strain[0], strain[1], strain[2], c=org_strain[idxs, 3])
+    axs.view_init(elev=0, azim=-90)
+    print(np.shape(strain))
+    print(np.shape(org_strain))
+    plt.show()
+    quit()
 
     strain = np.transpose(strain)
 
     # hist(strain[3])
-
+    print("start plotting...")
     fig = plt.figure()
     axs = fig.add_subplot(projection='3d')
-    plot = axs.scatter(strain[0], strain[1], strain[2], c=strain[3], s=5)
+    plot = axs.scatter(strain[0], strain[1], strain[2], c=clusterer.labels_, s=5)
     fig.colorbar(plot)
     plt.show()
 
+
 if __name__ == '__main__':
+    import sys
+    sys.path.insert(0, os.getcwd())
+    from polycorr.parse_xml import parse_xml
+    import polycorr.plotting as plotting
     main()
