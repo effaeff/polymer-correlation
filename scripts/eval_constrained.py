05e7ce01d48462f494f8e11fb495d3e96e742dcf
seko0497 <sebastian.kons@tu-dortmund.de>
2022-05-18 12:32:19 +0200

    compare similarity

--- scripts/eval_constrained.py
+++ scripts/eval_constrained.py
@@ -3,7 +3,10 @@ import numpy as np
 import os
 import pickle
 from collections import Counter
+from sklearn.metrics import adjusted_rand_score, fowlkes_mallows_score, v_measure_score
 from tabulate import tabulate
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import NearestNeighbors
 
 
 def main():
@@ -103,6 +106,87 @@ def main():
             headers=("Threshold", "PCKMeans", "Birch")
         )
     )
+
+    strain_scaler = MinMaxScaler()
+    points_scaler = MinMaxScaler()
+
+    points_s = points_scaler.fit_transform(points)
+    strain_s = strain_scaler.fit_transform(strain)
+
+    # Get nearest points
+    strain_nn = NearestNeighbors(n_neighbors=1, metric="euclidean")
+    strain_nn.fit(strain_s[:, :3])
+
+    strain_neighbors = strain_nn.kneighbors(points_s, return_distance=True)[1]
+
+    clusterings_dir = "results/clusterings"
+
+    clustering = "Birch"
+    key = "threshold"
+    values = [0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.15, 0.2, 0.25]
+
+    data = []
+
+    points_s = np.transpose(points_s)
+
+    for value in values:
+
+        scores = []
+
+        strain_clustering_path = (f"{clusterings_dir}/strain/{clustering}/"
+                                  f"{clustering}-{key}_{value}.pkl")
+
+        point_clustering_path = (f"{clusterings_dir}/PCKMeans/"
+                                 "PCKMeans-n_clusters_279.pkl")
+
+        with open(strain_clustering_path, "rb") as strain_clustering_file:
+            strain_clustering = pickle.load(strain_clustering_file)
+
+        with open(point_clustering_path, "rb") as point_clustering_file:
+            point_clustering = pickle.load(point_clustering_file)
+
+        # Select clustering of strains which correspond to points
+        point_clusters = point_clustering.labels_
+        strain_clusters = strain_clustering.labels_[strain_neighbors][:, 0]
+
+        plotting.compare3d_clustering(
+            point_clusters,
+            strain_clusters,
+            points_s,
+            clustering,
+            key,
+            value
+        )
+
+        scores.append(value)
+
+        scores.append(adjusted_rand_score(
+            point_clusters,
+            strain_clusters
+        ))
+        scores.append(fowlkes_mallows_score(
+            point_clusters,
+            strain_clusters
+        ))
+        scores.append(v_measure_score(
+            point_clusters,
+            strain_clusters
+        ))
+
+        data.append(scores)
+
+    headers = [
+        "threshold",
+        "adjusted_rand_score",
+        "fowlkes_mallows_score",
+        "v_measure_score"
+    ]
+
+    with open("results/similarity_scores_constrained.txt", "w+") as score_file:
+        score_file.write("Comparison between strain clustering over"
+                         "different birch-thresholds \nand point clustering "
+                         "with pckmeans with 279 clusters\n\n")
+        score_file.write(tabulate(data, headers=headers))
 
 
 if __name__ == '__main__':
